10ce81bfa95d7db05aa1c119c3caa37668dbfce8
Cizz22 <cisatraa@gmail.com>
2025-01-24 07:42:52 +0000

    fix: server

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -1,5 +1,5 @@
 # This file must be used with "source <venv>/bin/activate.fish" *from fish*
-# (https://fishshell.com/). You cannot run it directly.
+# (https://fishshell.com/); you cannot run it directly.
 
 function deactivate  -d "Exit virtual environment and return to normal shell environment"
     # reset old environment variables
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/perdanamain/Documents/it-practioners/fullstack-developer-tam/repositories/risk-matrix-scheduler/env
+set -gx VIRTUAL_ENV /home/aimo/digital-twin/risk-matrix-scheduler/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
